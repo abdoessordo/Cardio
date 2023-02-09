ac1f5261bd5219c5f401ddc33a098c8a3cd18abb
saadennigro <saadennigro@gmail.com>
2023-02-09 10:51:43 +0100

    bg

--- src/pages/welcome/welcome.js
+++ src/pages/welcome/welcome.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "../welcome/welcome.css";
 import img from "../../assets/Intro.jpg";
@@ -8,8 +8,11 @@ import logo2 from "../../assets/Logo CHU med VI.png";
 import { Link } from "react-router-dom";
 
 const Welcome = () => {
+  const [externs, setExterns] = useState(false);
+  const [department, setDepartement] = useState(false);
+
   return (
-    <>
+    <div className="container-parent">
       <div className="header-nav">
         <img src={logo1} alt="CHU Cardio" className="logo" />
         <h6 className="title">Cardiac evaluation before non-cardiac surgery</h6>
@@ -28,41 +31,22 @@ const Welcome = () => {
                 surgery, and which is based on the latest recommendations of
                 <b> l’European Society of Cardiology ‘’ESC’’ 2022.</b>
                 <br /> The site has been created as part of an internship
-                project by a group of externs supervised by the Cardiology
-                department of the{" "}
-                <b>CHU Mohammed VI Tangier-Tetouan-AlHoceima.</b>
+                project by a group of{" "}
+                <button
+                  className="externs"
+                  onClick={() => setExterns(!externs)}
+                >
+                  externs
+                </button>{" "}
+                supervised by the Cardiology{" "}
+                <button
+                  className="externs"
+                  onClick={() => setDepartement(!department)}
+                >
+                  department
+                </button>{" "}
+                of the <b>CHU Mohammed VI Tangier-Tetouan-AlHoceima.</b>
               </p>
-
-              <div className="names-wrapper">
-                <div className="names-1">
-                  <h6 className="names-title">1-Externes :</h6>
-                  <ul>
-                    <li>Boughlala Rajae</li>
-                    <li>Bouhjira Israe</li>
-                    <li>Bourimane Nouhaila</li>
-                    <li>Bourouhou Nouha</li>
-                    <li>Ouzid Maryam</li>
-                    <li>Chaarir Kaoutar</li>
-                  </ul>
-                </div>
-                <div className="names-2">
-                  <h6 className="names-title">2-Tuteurs :</h6>
-                  <ul>
-                    <li>
-                      <b>Chef de service :</b> <br />
-                      Professeur Raissouni Zainab
-                    </li>
-                    <li>
-                      <b>Professeur assistant :</b> <br />
-                      Professeur El Boussaadani Badr
-                    </li>
-                    <li>
-                      <b>Résident en cardiologie :</b> <br />
-                      Dr Berhil Taha
-                    </li>
-                  </ul>
-                </div>
-              </div>
 
               <div className="btn_wrapper">
                 <Link to="/survey">
@@ -72,13 +56,46 @@ const Welcome = () => {
             </div>
             <div className="grid-item-2">
               <div className="team_img_wrapper">
-                <img src={img} alt="team-img" />
+                <div className="names-wrapper">
+                  {externs && (
+                    <div className="names-1">
+                      <h6 className="names-title">1-Externes :</h6>
+                      <ul>
+                        <li>Boughlala Rajae</li>
+                        <li>Bouhjira Israe</li>
+                        <li>Bourimane Nouhaila</li>
+                        <li>Bourouhou Nouha</li>
+                        <li>Bouzid Maryam</li>
+                        <li>Chaarir Kaoutar</li>
+                      </ul>
+                    </div>
+                  )}
+                  {department && (
+                    <div className="names-2">
+                      <h6 className="names-title">2-Tuteurs :</h6>
+                      <ul>
+                        <li>
+                          <b>Chef de service :</b> <br />
+                          Professeur Raissouni Zainab
+                        </li>
+                        <li>
+                          <b>Professeur assistant :</b> <br />
+                          Professeur El Boussaadani Badr
+                        </li>
+                        <li>
+                          <b>Résident en cardiologie :</b> <br />
+                          Dr Berhil Taha
+                        </li>
+                      </ul>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           </div>
         </div>
       </section>
-    </>
+    </div>
   );
 };
 
