4e1d0470de501083b1d580941919ddefab10e07b
abdoessordo <abdoessordo01@gmail.com>
2023-02-10 21:08:21 +0100

    added google analytics

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Survey from "./pages/survey/survey";
 import Welcome from "./pages/welcome/welcome";
 
+// Google Analytics
 import ReactGA from "react-ga";
+import { Analytics } from "@vercel/analytics";
 
 const trackingId = "G-3FLJRD5MP5";
 ReactGA.initialize(trackingId);
@@ -12,13 +14,16 @@ ReactGA.initialize(trackingId);
 export default class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <Routes>
-          <Route index path="/" element={<Welcome />} />
-          <Route path="/survey" element={<Survey />} />
-          <Route path="*" element={<Welcome />} />
-        </Routes>
-      </BrowserRouter>
+      <>
+        <Analytics />
+        <BrowserRouter>
+          <Routes>
+            <Route index path="/" element={<Welcome />} />
+            <Route path="/survey" element={<Survey />} />
+            <Route path="*" element={<Welcome />} />
+          </Routes>
+        </BrowserRouter>
+      </>
     );
   }
 }
